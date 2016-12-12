36df692842ad7562e0d3c455547f1c0a14082350
Yin Li <eelregit@gmail.com>
2016-12-12 09:48:09 -0800

    changed N -> Ngal

--- io/qpm.c
+++ io/qpm.c
@@ -3,20 +3,20 @@
 #include <assert.h>
 
 
-void qpm_cubic_mocks_read(char *filename, int N, double *x, double *y, double *z,
+void qpm_cubic_mocks_read(char *catalog, int Ngal, double *x, double *y, double *z,
         double *vx, double *vy, double *vz, double *M, int *issat)
 {
-    x = (double *)malloc(N * sizeof(double)); assert(x!=NULL);
-    y = (double *)malloc(N * sizeof(double)); assert(y!=NULL);
-    z = (double *)malloc(N * sizeof(double)); assert(z!=NULL);
-    vx = (double *)malloc(N * sizeof(double)); assert(vx!=NULL);
-    vy = (double *)malloc(N * sizeof(double)); assert(vy!=NULL);
-    vz = (double *)malloc(N * sizeof(double)); assert(vz!=NULL);
-    M = (double *)malloc(N * sizeof(double)); assert(M!=NULL);
-    issat = (int *)malloc(N * sizeof(int)); assert(issat!=NULL);
+    x = (double *)malloc(Ngal * sizeof(double)); assert(x!=NULL);
+    y = (double *)malloc(Ngal * sizeof(double)); assert(y!=NULL);
+    z = (double *)malloc(Ngal * sizeof(double)); assert(z!=NULL);
+    vx = (double *)malloc(Ngal * sizeof(double)); assert(vx!=NULL);
+    vy = (double *)malloc(Ngal * sizeof(double)); assert(vy!=NULL);
+    vz = (double *)malloc(Ngal * sizeof(double)); assert(vz!=NULL);
+    M = (double *)malloc(Ngal * sizeof(double)); assert(M!=NULL);
+    issat = (int *)malloc(Ngal * sizeof(int)); assert(issat!=NULL);
 
-    FILE *fp = fopen(filename, "r"); assert(fp!=NULL);
-    for(int i=0; i<N; ++i){
+    FILE *fp = fopen(catalog, "r"); assert(fp!=NULL);
+    for(int i=0; i<Ngal; ++i){
         int ret = fscanf(fp, "%lf %lf %lf %lf %lf %lf %lf %d %*d\n",
                 x+i, y+i, z+i, vx+i, vy+i, vz+i, M+i, issat+i);
         assert(ret==8);
