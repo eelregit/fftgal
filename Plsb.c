b3f79684d6a67d8fde82af32cd75a7fc0789e10e
Yin Li <eelregit@gmail.com>
2017-01-14 11:57:06 -0800

    added real space power spectra to Plsb.c

--- Plsb.c
+++ Plsb.c
@@ -45,11 +45,12 @@ int main(int argc, char *argv[])
     double Lsb = L / Nsb;
     fftgal_t *fg = fftgal_init(Ng, Lsb, wisdom);
 
+    fprintf(stderr, "\n################## has rsd ##################\n");
     double *xd=NULL, *yd=NULL, *zd=NULL;
     double *xsb=NULL, *ysb=NULL, *zsb=NULL;
     for(int ilos=0; ilos<2; ++ilos)
     for(int jlos=0; jlos<2; ++jlos)
-    for(int klos=0; klos<2-(ilos==1 && jlos==1); ++klos){ /* skip los[] = {1,1,1} */
+    for(int klos=0+(ilos==0 && jlos==0); klos<2-(ilos==1 && jlos==1); ++klos){ /* skip {0,0,0}, {1,1,1} */
         fprintf(stderr, "================== los div ==================\n");
         if(xd!=NULL) free(xd);
         if(yd!=NULL) free(yd);
@@ -76,7 +77,7 @@ int main(int argc, char *argv[])
             fftgal_x2fk(fg, xsb, ysb, zsb, Np3sb);
 
             char outfile[maxlen];
-            ret = snprintf(outfile, maxlen, "%s/a%.4f_%04d/Pl_los%d%d%d_sb%d%d%d.txt",
+            ret = snprintf(outfile, maxlen, "%s/a%.4f_%04d/Pl_rsd1_los%d%d%d_sb%d%d%d.txt",
                     outdir, a, catid, ilos, jlos, klos, isb, jsb, ksb);
             assert(ret>=0 && ret<maxlen);
             double dK = 0.01;
@@ -84,6 +85,39 @@ int main(int argc, char *argv[])
         }
         assert(Np3sbtot==Np3);
     }
+
+    fprintf(stderr, "\n################ without rsd ################\n");
+    int Np3sbtot = 0;
+    for(int isb=0; isb<Nsb; ++isb)
+    for(int jsb=0; jsb<Nsb; ++jsb)
+    for(int ksb=0; ksb<Nsb; ++ksb){
+        fprintf(stderr, "================ sub-box div ================\n");
+        if(xsb!=NULL) free(xsb);
+        if(ysb!=NULL) free(ysb);
+        if(zsb!=NULL) free(zsb);
+        double xyzlim[6] = {isb*Lsb, (isb+1)*Lsb,
+                            jsb*Lsb, (jsb+1)*Lsb,
+                            ksb*Lsb, (ksb+1)*Lsb};
+        int Np3sb = subbox(x, y, z, Np3, xyzlim, &xsb, &ysb, &zsb, Np3);
+        Np3sbtot += Np3sb;
+
+        fftgal_x2fk(fg, xsb, ysb, zsb, Np3sb);
+
+        for(int ilos=0; ilos<2; ++ilos)
+        for(int jlos=0; jlos<2; ++jlos)
+        for(int klos=0+(ilos==0 && jlos==0); klos<2-(ilos==1 && jlos==1); ++klos){ /* skip {0,0,0}, {1,1,1} */
+            fprintf(stderr, "------------------ los div ------------------\n");
+            double los[3] = {ilos, jlos, klos};
+
+            char outfile[maxlen];
+            ret = snprintf(outfile, maxlen, "%s/a%.4f_%04d/Pl_rsd0_los%d%d%d_sb%d%d%d.txt",
+                    outdir, a, catid, ilos, jlos, klos, isb, jsb, ksb);
+            assert(ret>=0 && ret<maxlen);
+            double dK = 0.01;
+            Pl(fg, dK, los, outfile);
+        }
+    }
+    assert(Np3sbtot==Np3);
 
     free(x); free(y); free(z); free(vx); free(vy); free(vz); free(M); free(issat);
     free(xd); free(yd); free(zd);
