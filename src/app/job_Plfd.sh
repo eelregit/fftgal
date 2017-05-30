85da6e0b4a5aefd584d90e7b1fcf4b0911115d08
Yin Li <eelregit@gmail.com>
2017-05-30 19:47:19 +0900

    split fold and non-fold Pl job scripts

--- src/app/job_Plfd.sh
+++ src/app/job_Plfd.sh
@@ -2,14 +2,14 @@
 #SBATCH --ntasks=1
 #SBATCH --partition=shared
 #SBATCH --time=2:00:00
-#SBATCH --job-name=Pl
+#SBATCH --job-name=Plfd
 #SBATCH --output=Pl%j.out
 
 Pl=$SCRATCH/fftgal/Pl
-Ng=512
+Ng=128
 L=2560
 wisdom=${Ng}.wsdm
-fold=1
+fold=4
 catdir=/project/projectdirs/boss/galaxy/QPM/dr12d_cubic_mocks
 a=0.6452
 outdir=$SCRATCH/ssm.d
@@ -19,7 +19,7 @@ module load gcc fftw gsl
 make Pl
 for catid in $@
 do
-    log=$outdir/a${a}_$(printf '%04d' $catid)/Pl.log
+    log=$outdir/a${a}_$(printf '%04d' $catid)/Plfd.log
     time $Pl $Ng $L $wisdom $fold $catdir $a $catid $outdir 2> $log
 done
 echo ${SLURM_JOB_ID} ending $(date)
